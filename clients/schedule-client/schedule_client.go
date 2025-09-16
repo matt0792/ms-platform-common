1285b0fe7361d3998f96aa0c095a75bf02d54e91
Matt Thompson <matthew.thompson6@icloud.com>
2025-09-16 21:10:49 +0200

    scheduleclient

--- clients/schedule-client/schedule_client.go
+++ clients/schedule-client/schedule_client.go
@@ -1,6 +1,10 @@
 package scheduleclient
 
-import "github.com/matt0792/mscommon/s2s"
+import (
+	schedulemodels "github.com/matt0792/ms-platform-common/models/schedule-models"
+	"github.com/matt0792/mscommon/commonmodels"
+	"github.com/matt0792/mscommon/s2s"
+)
 
 type ScheduleClient struct {
 	s2s.Transport
@@ -13,4 +17,38 @@ func NewScheduleClient(baseUrl, token string) *ScheduleClient {
 			Token:   token,
 		},
 	}
+}
+
+func (c *ScheduleClient) GetCronJob(id string) (schedulemodels.CronJob, error) {
+	var resp commonmodels.Response[schedulemodels.CronJob]
+	err := c.Call("GET", "/cron/"+id, nil, &resp)
+	return s2s.UnwrapResponse(resp, err)
+}
+
+func (c *ScheduleClient) GetAllCronJobs() ([]schedulemodels.CronJob, error) {
+	var resp commonmodels.Response[[]schedulemodels.CronJob]
+	err := c.Call("GET", "/cron/all", nil, &resp)
+	return s2s.UnwrapResponse(resp, err)
+}
+
+func (c *ScheduleClient) GetActiveCronJobs() ([]schedulemodels.CronJob, error) {
+	var resp commonmodels.Response[[]schedulemodels.CronJob]
+	err := c.Call("GET", "/cron/active", nil, &resp)
+	return s2s.UnwrapResponse(resp, err)
+}
+
+func (c *ScheduleClient) SetCronJob(job schedulemodels.CronJob) (string, error) {
+	var resp commonmodels.Response[string]
+	err := c.Call("POST", "/cron/create", job, &resp)
+	return s2s.UnwrapResponse(resp, err)
+}
+
+func (c *ScheduleClient) SetCronJobActive(id string, active bool) (string, error) {
+	body := struct {
+		IsActive bool `json:"isActive"`
+	}{IsActive: active}
+
+	var resp commonmodels.Response[string]
+	err := c.Call("PATCH", "/cron/"+id+"/activate", body, &resp)
+	return s2s.UnwrapResponse(resp, err)
 }
